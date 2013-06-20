11732c0751367d57a8b017283a7c6991d5d8abfb
Thomas Bores <thomas.bores@gmail.com>
2013-06-20 10:03:37 +0200

    pom.xml: - Remove SNAPSHOT from version number - Correct the groupId of sonar-xml-plugin Copy_necessary_jars_in_maven_locally: - Remove all reference to local sonar-xml-plugin.jars as it is now downloaded from central maven repository

--- copy_necessary_jars_in_maven_locally.bat
+++ copy_necessary_jars_in_maven_locally.bat
@@ -2,4 +2,3 @@
 call mvn install:install-file -Dfile=./scripts/lint_api-r17.jar -DpomFile=pom.xml -DgroupId=com.android.tools.lint -DartifactId=lint_api -Dversion=r17 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
 call mvn install:install-file -Dfile=./scripts/lint-r17.jar -DpomFile=pom.xml -DgroupId=com.android.tools.lint -DartifactId=lint -Dversion=r17 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
 call mvn install:install-file -Dfile=./scripts/lint_checks-r17.jar -DpomFile=pom.xml -DgroupId=com.android.tools.lint.checks -DartifactId=lint_checks -Dversion=r17 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
-REM call mvn install:install-file -Dfile=./scripts/sonar-xml-plugin-1.0.jar -DpomFile=pom.xml -DgroupId=org.sonar.plugins.xml -DartifactId=sonar-xml-plugin -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
