69545934ec47f771e43c5e658e5a69a60dd4eddb
Thomas Bores <thomas.bores@gmail.com>
2013-06-28 10:18:23 +0200

    Remove the Android Lint Tool jars from the scripts repository and the according script to install them locally in your Maven.

--- copy_necessary_jars_in_maven_locally.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call mvn install:install-file -Dfile=./scripts/lint_api-r17.jar -DpomFile=pom.xml -DgroupId=com.android.tools.lint -DartifactId=lint_api -Dversion=r17 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
-call mvn install:install-file -Dfile=./scripts/lint-r17.jar -DpomFile=pom.xml -DgroupId=com.android.tools.lint -DartifactId=lint -Dversion=r17 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
-call mvn install:install-file -Dfile=./scripts/lint_checks-r17.jar -DpomFile=pom.xml -DgroupId=com.android.tools.lint.checks -DartifactId=lint_checks -Dversion=r17 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
