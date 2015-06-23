fa2feaf3b87099c9b7a857d52087719012914697
David Gageot <david@gageot.net>
2015-06-23 14:16:48 +0200

    Add ITs

---
+++ its/plugin/projects/SonarAndroidSample/app/src/main/java/org/sonarsource/app/TestViolations.java
@@ -0,0 +1,12 @@
+package org.sonarsource.app;
+
+import android.R;
+import android.os.StrictMode;
+
+public class TestViolations {
+
+    private void method(){
+
+        StrictMode.enableDefaults();
+    }
+}
