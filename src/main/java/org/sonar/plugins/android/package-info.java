2490e26c899a4fc4c0589511fc8c23d9d37bd996
David Rautureau <david.rautureau@sonarsource.com>
2016-05-23 18:01:16 +0200

    BUILD-82 Use parent 32
    Inherit the deploy-sonarsource profile to deploy to repox

--- src/main/java/org/sonar/plugins/android/package-info.java
+++ src/main/java/org/sonar/plugins/android/package-info.java
@@ -1,7 +1,7 @@
 /*
  * SonarQube Android Lint Plugin
- * Copyright (C) 2013 SonarSource and Jerome Van Der Linden, Stephane Nicolas, Florian Roncari, Thomas Bores
- * dev@sonar.codehaus.org
+ * Copyright (C) 2013-2016 SonarSource SA  and Jerome Van Der Linden, Stephane Nicolas, Florian Roncari, Thomas Bores
+ * sonarqube@googlegroups.com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -13,9 +13,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 @javax.annotation.ParametersAreNonnullByDefault
 package org.sonar.plugins.android;
